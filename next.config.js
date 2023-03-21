2a93437eda0010e906aa68791526744d89ce4a17
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-03-22 00:31:43 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-    "rules": {
+    "extends": "next",
+  "rules": {
     "react/no-unescaped-entities": "off",
     "@next/next/no-page-custom-font": "off"
   }
