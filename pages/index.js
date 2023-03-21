53a938239cc2aa90bf93d2ff207893fce89be1e1
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-03-22 00:19:45 +0300

    profile

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,35 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-        <div><ul>
-        <li>Home</li>
-        <li>Pages</li>
-        <li>ayman</li>
-        <li></li>
-        <li></li></ul></div>
+       <div className="profile">
+      <div className="i-profile">
+        <img src="/scoir2.svg" alt="" className="lain-1" width={50} />
+        <div className="i-me">
+          <h1>Hi my name is Ayman Elmosuy , I'am Web Developer</h1>{" "}
+          <img
+            src="/shak.svg"
+            width={500}
+            alt=""
+            className="lain"
+            draggable="false"
+          />
+          <img
+            src="/scoir.svg"
+            width={100}
+            draggable="false"
+            className="scoir-profile"
+            alt=""
+          />
+          <p>
+            work in Full stack Developer Front End developer and Back End
+            developer
+          </p>
+        </div>{" "}
+      </div>
+
+      <div>
+        <img src="/ayman.png" className="img-me" alt="" width={300} />
+      </div>
     </div>
   )
 }
