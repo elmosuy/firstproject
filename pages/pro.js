fbce2f764b5a4453c28ee0b2b14efb786f37007a
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-03-22 02:06:50 +0300

    Update pro.js

--- pages/pro.js
+++ pages/pro.js
@@ -1,31 +1,14 @@
 import React from "react";
-export const getStaticPaths = async () => {
-  const res = await fetch(
-    "https://jsonplaceholder.typicode.com/posts/post"
-  );
-  const data = await res.json();
-  const paths = data.map((post) => {
-    return {
-      params: { id: post.id.toString() },
-    };
-  });
-  return { paths, fallback: false };
-};
 
-export const getStaticProps = async (context) => {
-  const id = context.params.id;
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts/"+"post/" + id);
-  const data = await res.json();
 
-  return { props: { post: data } };
-};
 
-const Post = ({ post }) => {
+
+const Post = () => {
   return (
     <div className="container py-5">
-      <h2>{post.title}</h2>
+      <h2>Ayman Haider</h2>
       <hr />
-      <p>{post.body}</p>
+      <p>front end developer and back end developer can speak english and arabic</p>
     </div>
   );
 };
