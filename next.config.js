d2678bb134502b57f0b5e89ed4c68d1cec21b381
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-03-22 00:45:49 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
  "extends": "next",
   "rules": {
     "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off"
+    "@next/next/no-page-custom-font": "off",
   },
 }
 
