67d469ffca9fa523523576b22728720debaccc11
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-03-22 01:10:42 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
        <div className="profile">
-        <h1>الحقيرة</h1>
+        <h1>الحقيرة هسة صار ساعة سودة الشفتيني بيها </h1>
 
    
     </div>
