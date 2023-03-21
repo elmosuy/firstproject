38f22333af1da79477117e6ec13f8ab887a2dab0
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-03-22 01:59:57 +0300

    Update layout.js

--- pages/layout.js
+++ pages/layout.js
@@ -1,7 +1,5 @@
 import Link from "next/link";
 import React,{useEffect, useRef, useState} from "react"
-import { useContext } from "react";
-import { motion } from "framer-motion";
 const Layout = ({children}) => {
 
  
@@ -36,67 +34,18 @@ function navre(){
 
   return (
     <div >
-     <div className="header"   style={{background:`linear-gradient(to left ,#e66465 )` }}>
-        <motion.h1 drag dragConstraints={{bottom:10,top:1,left:10,right:1} }>Moon</motion.h1>
-          
-        
-      </div>
-        <header> 
- 
-
- <ul className="linknav">
+     <div>
+   <ul className="linknav">
          <li><Link href='/' ><a download> Home</a></Link></li>
-         <li><Link href='/appCv' ><a > Workied</a></Link></li>
+         <li><Link href='/pro' ><a > Workied</a></Link></li>
          <li><Link href='/store' ><a > Store</a></Link> </li>
-         <li onClick={handellog } className="loginf">Login</li>
-           <div className="search" dir="rtl" title="searching" >  <input type="search" placeholder="ابحث..." name="" id="" /></div>
+         <li  className="loginf">Login</li>
+           <div className="search" dir="rtl" title="searching" >
+            <input type="search" placeholder="ابحث..." name="" id="" />
+          </div>
  </ul>
-      <section className='burg' onClick={navb}><p>|</p> <p>|</p><p>|</p></section>
-      <nav className='navnar' id='navbar'style={{background:`linear-gradient(to left,red ,rgba(0, 17, 255,0) )` }}  >
-                        
-   
-             
-      <ul className="navbar">
-             <div dir="rtl" className="search" title="searching">  <input type="search" placeholder="بحث..." name="" id="" /></div>
-         <li><Link href='/' ><a > Home</a></Link></li>
-         <li><Link href='/appCv' ><a > Workied </a></Link></li>
-         <li><Link href='/store' ><a > Store</a></Link> </li>
-         <li><Link href='/Qustion' ><a > Qustion</a></Link> </li>
-          
-      </ul>
-       <div className="setcolor">
-       <p>set color</p>
-       <input type="color"  placeholder="Set Color" name="" id="" />
-       <div onClick={handello } className="logi">Login</div>
-
-      </div>
- 
-</nav></header>
-<section className="list"  id="log">
-  <h1>Login</h1>
-  <form action="" method="get" >
-    
-    <input placeholder="Email / User Name"  type="text" required/>
-    <input type="password" placeholder="Password" required/>
-    <p dir="rtl" style={{fontSize:'13px',marginRight:'20px',color:'#086fee',cursor:'pointer'}}>هل نسيت كلمة السر؟</p>
-    <button type="submit">تسجيل الدخول</button>
-  </form>
-</section>
-    <div className="media">
-     
-   <a href="https://facebook.com/"> Facebook</a>
-  <a href="https://www.instagram.com/">  instagram</a>
-    <a href="https://twitter.com/login">twitter</a>
-   <a href="https://www.linkedin.com/"> linkdin</a>
-  
-  
-  
-   {/* <p className="note">set Color</p>  */}
-
-
-  </div>
-  <div  className='style.container' >
-    {children} </div> 
+   </div>
+    {children} 
   
     </div>
   )
