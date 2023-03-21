4a1c7e6ab646ea0175cfefdffde3a3b59e523b40
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-03-22 00:49:19 +0300

    Update index.js
    edit and deleted img becuse bug in debloy

--- pages/index.js
+++ pages/index.js
@@ -6,23 +6,10 @@ export default function Home() {
   return (
        <div className="profile">
       <div className="i-profile">
-        <img src="/scoir2.svg" alt="" className="lain-1" width={50} />
         <div className="i-me">
           <h1>Hi my name is Ayman Elmosuy , I'am Web Developer</h1>{" "}
-          <img
-            src="/shak.svg"
-            width={500}
-            alt=""
-            className="lain"
-            draggable="false"
-          />
-          <img
-            src="/scoir.svg"
-            width={100}
-            draggable="false"
-            className="scoir-profile"
-            alt=""
-          />
+       
+       
           <p>
             work in Full stack Developer Front End developer and Back End
             developer
@@ -31,7 +18,6 @@ export default function Home() {
       </div>
 
       <div>
-        <img src="/ayman.png" className="img-me" alt="" width={300} />
       </div>
     </div>
   )
