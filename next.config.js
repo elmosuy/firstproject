7d396dcaf1be151c672e9d66c53d25f717822822
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-03-22 00:34:54 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-    "extends": "next",
+  eslint: {
+    dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
+  { "extends": "next",
   "rules": {
     "react/no-unescaped-entities": "off",
     "@next/next/no-page-custom-font": "off"
-  }
+  },
 }
 
 module.exports = nextConfig
