1e243e34549dbc15fc5926198bf07e2a4fe5e72c
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-03-22 01:21:32 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
-export default function Home() {
+import Link from 'next/link';
+ 
+export default function Home({posts}) {
   return (
-       <div className="profile">
-        <h1>الحقيرة هسة صار ساعة سودة الشفتيني بيها </h1>
-    <h2>انجبي</h2>
-
-   
+    <div className={styles.container}>
+     { posts.map((post)=>(
+      <Link href={`post/${post.id}`} key={post.body}><h1 >{post.title}</h1></Link>
+     ))}
     </div>
   )
 }
+export async function getStaticProps(){
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const data = await res.json();
+  return{
+    props:{
+      posts:data
+    }
+  }}
