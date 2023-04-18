513de5476877bf6a422eb160e7300b4b82b92678
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-04-19 01:21:24 +0300

    Update [id].js

--- pages/components/Pages/Page_1/[id].js
+++ pages/components/Pages/Page_1/[id].js
@@ -5,9 +5,22 @@ import { useState } from "react";
 import useFetchTrendingCatagory from "../../hooks/useFetchTrendingCatagory";
 import useFetchTopRatedCatagory from "../../hooks/useFetchTopRatedCatagory";
 
+const useMovies = (genre) => {
+  switch (id) {
+    case 'Trending':
+      return useFetchTrendingCatagory()
+    case 'Top Rated"':
+      return useFetchTopRatedCatagory()
+    default:
+      return useFetchMovieGenreResults(id)
+  }
+}
+
 const item1 = () => {
     const router = useRouter()
     const { id } = router.query
+   const mymovies = useMovies(genre)
+
   return (
     <div  className='texts'>
       <div className='text'>
