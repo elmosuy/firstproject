ca7ac2cd169d3f968c6185664ea05e3b668e36ff
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-04-21 22:24:53 +0300

    Add files via upload

--- pages/me.js
+++ pages/me.js
@@ -29,14 +29,14 @@ const About = () => {
       </li>
       <li  id='anim2'>
         <div class="timeline-content animtion" >
-          <h3 class="date">20th Apr, 2023</h3>
+          <h3 class="date">5th Apr, 2023</h3>
           <h1>website Dream</h1>
           <p>موقع عربي لتفسير الاحلام مجانا وغير ربحي ذات طابع مميز لانه فيه العديد من المميزات ف منها خفيف وسريع بدون اعلانات مزعجة فيه اكثر من اقسام عديدة دينية يعمل على جميع الاجهزة الصغيرة والكبيرة </p>
         </div>
       </li>
       <li>
         <div class="timeline-content" id='anim3'>
-          <h3 class="date">20th may, 2010</h3>
+          <h3 class="date">13th may, 2022</h3>
           <h1>App Scraping</h1>
           <p>تطبيق يمكن سحب اي بيانات من المواقع الالكترونية ليستفاد منه طلاب علوم الحاسبات في تحليل البيانات </p>
         </div>
@@ -46,63 +46,54 @@ const About = () => {
       </li>
       <li>
         <div class="timeline-content" id='anim4'>
-          <h3 class="date">20th may, 2010</h3>
           <h1>React js</h1>
           <p></p>
         </div>
       </li>
       <li>
         <div class="timeline-content" id='anim3'>
-          <h3 class="date">20th may, 2010</h3>
           <h1> Next Js</h1>
           <p></p>
         </div>
       </li>
       <li>
         <div class="timeline-content" id='anim4'>
-          <h3 class="date">20th may, 2010</h3>
           <h1>Node Js</h1>
           <p></p>
         </div>
       </li>
       <li>
         <div class="timeline-content" id='anim3'>
-          <h3 class="date">20th may, 2010</h3>
           <h1>Express</h1>
           <p></p>
         </div>
       </li>
       <li>
         <div class="timeline-content" id='anim4'>
-          <h3 class="date">20th may, 2010</h3>
           <h1>MongoDB</h1>
           <p></p>
         </div>
       </li>
       <li>
         <div class="timeline-content" id='anim3'>
-          <h3 class="date">20th may, 2010</h3>
           <h1>My SQL</h1>
           <p></p>
         </div>
       </li>
       <li>
         <div class="timeline-content" id='anim4'>
-          <h3 class="date">20th may, 2010</h3>
           <h1>Web Scraping</h1>
           <p></p>
         </div>
       </li>
       <li>
         <div class="timeline-content" id='anim3'>
-          <h3 class="date">20th may, 2010</h3>
           <h1>TypeScript</h1>
           <p> </p>
         </div>
       </li>
       <li>
         <div class="timeline-content" id='anim4'>
-          <h3 class="date">20th may, 2010</h3>
           <h1>Jwt</h1>
           <p></p>
         </div>
