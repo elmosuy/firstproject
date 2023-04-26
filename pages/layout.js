9ec85ebd2294a02c7c550a4a39efccf3418c70af
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-04-26 22:37:37 +0300

    Update layout.js

--- pages/layout.js
+++ pages/layout.js
@@ -54,6 +54,17 @@ const Layout = ({children}) => {
         <div  dir='rtl'>
             <Letters handelBorgeremove={handelBorgeremove} />
         </div>
+          
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9186682855877789"
+     crossorigin="anonymous"></script>
+    <ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="autorelaxed"
+     data-ad-client="ca-pub-9186682855877789"
+     data-ad-slot="7222832903"></ins>
+       <script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 
         
 
