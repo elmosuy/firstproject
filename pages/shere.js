264d4e873aea379bd7e9748a6105f3e565fb848e
elmosuy <120755987+elmosuy@users.noreply.github.com>
2023-06-17 12:25:16 +0300

    Update shere.js

--- pages/shere.js
+++ pages/shere.js
@@ -7,9 +7,7 @@ const Shere = () => {
         <div className='n1'>
             <h2>جاري العمل على هذه الصفحة</h2>
         </div>
-        <div className='n2'>
-        <Image src='/searc.svg' width={500}  height={500}/>
-        </div>
+       
     </div>
   )
 }
